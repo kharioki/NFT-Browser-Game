d6136ce922ca07508419c8ae3b199c6a97afb142
tony <antolisah@gmail.com>
2021-12-05 23:59:41 +0300

    first contract

---
+++ contracts/MyEpicGame.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract MyEpicGame {
+    constructor() {
+        console.log("Let's play a game!");
+    }
+}
